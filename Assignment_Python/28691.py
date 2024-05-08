cd99bebfc9c5778a8243e22293ea2823b534b011
shy3180 <suhheeyang@gmail.com>
2024-05-08 14:19:00 +0900

    2024.05.08

---
+++ Assignment_Python/28691.py
@@ -0,0 +1,16 @@
+N = input()
+
+if N == "M":
+    print("MatKor")
+
+elif N == "W":
+    print("WiCys")
+
+elif N == "C":
+    print("CyKor")
+
+elif N == "A":
+    print("AlKor")
+
+elif N == "$":
+    print("$clear")
