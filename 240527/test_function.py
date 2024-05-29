36e29ac552fda938f62cad7ad01453d0b456465f
shy3180 <suhheeyang@gmail.com>
2024-05-29 18:54:01 +0900

    240529

---
+++ 240527/test_function.py
@@ -0,0 +1,9 @@
+import pip
+
+
+def test_function():
+    assert (1, 2, 3) == (1, 2, 3)
+
+
+def test_function2():
+    assert (1, 2, 3) == (1, 4, 3)
---
+++ 240527/test_p1000.py
@@ -0,0 +1,6 @@
+import pytest
+from p1000 import add
+
+
+def test_add():
+    assert add(1, 2) == 3
